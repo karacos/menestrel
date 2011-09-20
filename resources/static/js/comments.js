d9fc6a6ae11f8392fe18970c199dc77231acfd6a
nka11 <nka11@nka.me>
2011-09-21 01:55:37 +0200

    Starting comments ui

---
+++ resources/static/js/comments.js
@@ -0,0 +1,35 @@
+console.log("inside comments script");
+
+function createCommentsObject(){
+	KaraCos.comments = {
+		'initialize': function(res_url,container) {
+			if (KaraCos.auth.hasAction('get_comments')) {
+				KaraCos.action({
+					url: 'res_url',
+					action: 'get_comments',
+					params: {},
+					callback: function(data) {
+						$.ajax({
+							url: "/fragment/comments_list.jst",
+							context: document.body,
+							type: "GET",
+							cache: false,
+							success: function(tmpl) {
+								var comments_list_template = jsontemplate.Template(tmpl, KaraCos.jst_options);
+								container.empty().append(comments_list_template.expand(data));
+							}
+						});
+					},
+					error: function(data) {
+						
+					}
+				});
+			}
+		}
+	}
+}
+KaraCos(function(){
+	if (typeof KaraCos.comments === "undefined") {
+		createCommentsObject();
+	}
+});
