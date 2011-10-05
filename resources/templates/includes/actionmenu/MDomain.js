21cd8f10efc374a0732db2713e093edcf2ad91df
nka11 <nka11@nka.me>
2011-10-05 07:51:17 +0200

    reviewed jquery linking

--- resources/templates/includes/actionmenu/MDomain.js
+++ resources/templates/includes/actionmenu/MDomain.js
@@ -21,46 +21,47 @@
 		item,
 		actionwindow = KaraCos.actionMenu.actionWindow;
 	% if '_settings' in node_actions:
-		item = KaraCos('<li><a href="#">Parametrage</a></li>');
-	item.click(function(){
-				KaraCos.getForm({
-					url: "${instance.__domain__._get_action_url()}",
-					form: "_settings",
-					callback: function(data, form) {
-						var create_child_node_template = jsontemplate.Template(form, KaraCos.jst_options);
-						actionwindow.empty().append(create_child_node_template.expand(data));
-						actionwindow.dialog({width: '600px', modal:true,buttons: [{
-							'text': data.form.submit,
-							'click': function() {
-								var params = {},
-									method = '_settings';
-								$.each(actionwindow.find('form').serializeArray(), function(i, field) {
-									if (field.name === "method") {
-										method = field.value;
-									} else {
-										params[field.name] = field.value;
-									}
-								}); // each
-								KaraCos.action({ url: "${instance.__domain__._get_action_url()}",
-									method: method,
-									async: false,
-									params: params,
-									callback: function(data) {
-										if (data.success) {
-											
-											actionwindow.dialog('close');
-											
-										}
-									},
-									error: function(data) {
-										
-									}
-								}); // POST login form
-							}
-						}]}).show();
-					}
-				});
-			});
+		item = KaraCos('<li><button>Parametrage</button></li>');
+	item.find('button').button().click(actionsMenu.getActionFormButtonHandler("${instance.__domain__._get_action_url()}",'_settings'));
+//			function(){
+//				KaraCos.getForm({
+//					url: "${instance.__domain__._get_action_url()}",
+//					form: "_settings",
+//					callback: function(data, form) {
+//						var create_child_node_template = jsontemplate.Template(form, KaraCos.jst_options);
+//						actionwindow.empty().append(create_child_node_template.expand(data));
+//						actionwindow.dialog({width: '600px', modal:true,buttons: [{
+//							'text': data.form.submit,
+//							'click': function() {
+//								var params = {},
+//									method = '_settings';
+//								$.each(actionwindow.find('form').serializeArray(), function(i, field) {
+//									if (field.name === "method") {
+//										method = field.value;
+//									} else {
+//										params[field.name] = field.value;
+//									}
+//								}); // each
+//								KaraCos.action({ url: "${instance.__domain__._get_action_url()}",
+//									method: method,
+//									async: false,
+//									params: params,
+//									callback: function(data) {
+//										if (data.success) {
+//											
+//											actionwindow.dialog('close');
+//											
+//										}
+//									},
+//									error: function(data) {
+//										
+//									}
+//								}); // POST login form
+//							}
+//						}]}).show();
+//					}
+//				});
+//			});
 	submenu.append(item);
 	%endif
 })(submenu);
