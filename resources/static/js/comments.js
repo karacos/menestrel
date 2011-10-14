3778799f025d125d14b60fa3e87788e72030f6bd
nka11 <nka11@nka.me>
2011-10-14 09:02:36 +0200

    Working comments.js with requirejs

--- resources/static/js/comments.js
+++ resources/static/js/comments.js
@@ -1,37 +1,40 @@
-function createCommentsObject(){
-	KaraCos.comments = {
-		'initialize': function(res_url,container) {
-			if (KaraCos.auth.hasAction('get_comments')) {
-				KaraCos.action({
-					url: 'res_url',
-					action: 'get_comments',
-					params: {},
-					callback: function(data) {
-						$.ajax({
-							url: "/fragment/comments_list.jst",
-							context: document.body,
-							type: "GET",
-							cache: false,
-							success: function(tmpl) {
-								var comments_list_template = jsontemplate.Template(tmpl, KaraCos.jst_options);
-								container.empty().append(comments_list_template.expand(data));
-							}
-						});
-					},
-					error: function(data) {
-						
-					}
-				});
-			}
+define('menestrel/comments',['jquery', 'karacos/main'], function($,karacos) {
+	var 
+		auth,
+		comments = {
+				'initialize': function(res_url,container) {
+					$('body').bind('kcauth', function() {
+						auth = karacos.authManager;
+						if (auth.hasAction('get_comments')) {
+							karacos.action({
+								url: 'res_url',
+								action: 'get_comments',
+								params: {},
+								callback: function(data) {
+									$.ajax({
+										url: "/fragment/comments_list.jst",
+										context: document.body,
+										type: "GET",
+										cache: false,
+										success: function(tmpl) {
+											var comments_list_template = jsontemplate.Template(tmpl, KaraCos.jst_options);
+											container.empty().append(comments_list_template.expand(data));
+										}
+									});
+								},
+								error: function(data) {
+									
+								}
+							});
+						}
+					});
+				}
+			};
+	$('body').bind('kcready', function(){
+		karacos = KaraCos;
+		if (typeof karacos.comments === "undefined") {
+			karacos.comments = comments;
 		}
-	}
-}
-require(["karacos/jquery"],function($){
-	$('body').bind('karacosCoreLoaded', function(){
-		KaraCos(function(){
-			if (typeof KaraCos.comments === "undefined") {
-				createCommentsObject();
-			}
-		})
 	});
+	return comments;
 });
