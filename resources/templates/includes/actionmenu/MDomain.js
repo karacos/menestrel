86efb06727090bf909d51bf5525d13f932e798e8
nka11 <nka11@nka.me>
2011-10-25 22:12:00 +0200

    reviewed settings form, and templating

--- resources/templates/includes/actionmenu/MDomain.js
+++ resources/templates/includes/actionmenu/MDomain.js
@@ -19,51 +19,64 @@
 (function mDomainSubMenu(submenu){
 	var 
 		item,
-		actionwindow = KaraCos.actionMenu.actionWindow;
-	% if '_settings' in node_actions:
+		actionwindow = KaraCos.actionWindow;
+	if (auth.hasAction('_settings')) {
 		item = KaraCos('<li><button>Parametrage</button></li>');
-	item.find('button').button().click(actionsMenu.getActionFormButtonHandler("${instance.__domain__._get_action_url()}",'_settings'));
-//			function(){
-//				KaraCos.getForm({
-//					url: "${instance.__domain__._get_action_url()}",
-//					form: "_settings",
-//					callback: function(data, form) {
-//						var create_child_node_template = jsontemplate.Template(form, KaraCos.jst_options);
-//						actionwindow.empty().append(create_child_node_template.expand(data));
-//						actionwindow.dialog({width: '600px', modal:true,buttons: [{
-//							'text': data.form.submit,
-//							'click': function() {
-//								var params = {},
-//									method = '_settings';
-//								$.each(actionwindow.find('form').serializeArray(), function(i, field) {
-//									if (field.name === "method") {
-//										method = field.value;
-//									} else {
-//										params[field.name] = field.value;
-//									}
-//								}); // each
-//								KaraCos.action({ url: "${instance.__domain__._get_action_url()}",
-//									method: method,
-//									async: false,
-//									params: params,
-//									callback: function(data) {
-//										if (data.success) {
-//											
-//											actionwindow.dialog('close');
-//											
-//										}
-//									},
-//									error: function(data) {
-//										
-//									}
-//								}); // POST login form
-//							}
-//						}]}).show();
-//					}
-//				});
-//			});
-	submenu.append(item);
-	%endif
+		item.find('button').button().click(
+			function(){
+				var 
+					formData = auth.getForm('_settings'),
+					template;
+				$.ajax({
+					url: "/_browser/karacos-ui/core/form.tmpl.html",
+					async: false,
+					success: function(data) {
+						template = data;
+					}
+				});
+				actionwindow.empty().append($.tmpl(template,formData));
+				actionwindow.dialog({
+					width: '600px',
+					modal:true,
+					buttons: [
+					  {
+						  'text': "Annuler",
+						  'click': function(event) {
+							  actionwindow.dialog('close');
+						  }
+					  },
+					  {
+						  'text': formData.form.submit,
+						  'click': function(event) {
+							var params = {},
+								method = formData.action;
+							$.each(actionwindow.find('form').serializeArray(), function(i, field) {
+								if (field.name === "method") {
+									method = field.value;
+									} else {
+										params[field.name] = field.value;
+									}
+								}); // each
+								KaraCos.action({ url: "${instance.__domain__._get_action_url()}",
+									method: method,
+									async: false,
+									params: params,
+									callback: function(data) {
+										if (data.success) {
+											actionwindow.dialog('close');
+										}
+									},
+									error: function(data) {
+										
+									}
+								}); // POST login form
+							}
+						},
+						]}).show();
+		
+			});
+			submenu.append(item);
+		}
 })(submenu);
 % endif
 % except:
